7ebade2e1d1a0d240fef61e0a13cae103c23e2a5
chain <chain@garageplug.com>
2022-10-07 01:40:47 +0530

    inital commit

---
+++ src/test/java/com/example/spring5firstproject/Spring5firstprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring5firstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5firstprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
